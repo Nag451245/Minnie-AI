59bb977cb033e489027b32f6b885925eec68e6d1
Nag451245 <nagendra.bpuchala@gmail.com>
2026-02-03 22:49:24 +0530

    Fix: Add missing Log import to StepCounterModule

--- android/app/src/main/java/com/minnieai/StepCounterModule.kt
+++ android/app/src/main/java/com/minnieai/StepCounterModule.kt
@@ -5,6 +5,7 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.util.Log
 import com.facebook.react.bridge.*
 import com.facebook.react.modules.core.DeviceEventManagerModule
 
